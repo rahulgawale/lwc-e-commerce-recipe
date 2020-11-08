5f9a3147336cdf1b0802904a6d1fe507dd5a7ac0
Rahul Gawale <rahul.g.jack@gmail.com>
2020-11-08 21:37:04 +0530

    added code for Lightning Messaging Service

--- force-app/main/default/lwc/productFilters/productFilters.js
+++ force-app/main/default/lwc/productFilters/productFilters.js
@@ -1,5 +1,51 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
 
+// messageChannels
+import { publish, MessageContext } from "lightning/messageService";
+import FILTER_CHANNEL from "@salesforce/messageChannel/ProductFilterChannel__c";
+
+// fields
+import FAMILY_FIELD from "@salesforce/schema/Product2.Family";
+import SIZES_FIELD from "@salesforce/schema/Product2.Sizes__c";
 export default class ProductFilters extends LightningElement {
-    
+	families = [];
+	minPrice;
+	maxPrice;
+	brand;
+	size;
+
+	@wire(getPicklistValues, {
+		recordTypeId: "012000000000000AAA",
+		fieldApiName: FAMILY_FIELD
+	})
+	familyPicklistValues;
+
+	@wire(getPicklistValues, {
+		recordTypeId: "012000000000000AAA",
+		fieldApiName: SIZES_FIELD
+	})
+	sizesPicklistValues;
+
+	@wire(MessageContext)
+	messageContext;
+
+	handleChange(event) {
+		this[event.target.name] = event.detail.value;
+		console.log(event.detail.value);
+		this.publishChange();
+	}
+
+	publishChange() {
+		const message = {
+			filtersData: {
+				families: this.families,
+				minPrice: this.minPrice,
+				maxPrice: this.maxPrice,
+				size: this.size,
+				brand: this.brand
+			}
+		};
+		publish(this.messageContext, FILTER_CHANNEL, message);
+	}
 }
--- force-app/main/default/lwc/productSearch/productSearch.js
+++ force-app/main/default/lwc/productSearch/productSearch.js
@@ -1,11 +1,57 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, wire, track } from "lwc";
+
+// messageChannels
+import {
+	subscribe,
+	unsubscribe,
+	MessageContext
+} from "lightning/messageService";
+import FILTER_CHANNEL from "@salesforce/messageChannel/ProductFilterChannel__c";
+
+// apex
 import getProducts from "@salesforce/apex/ProductService.getProducts";
+
 export default class ProductSearch extends LightningElement {
-	searchKey;
+	searchKey = "";
+	subscription = null;
+	receivedMessage;
+	
+	@track filtersData = {};
+	
+	@wire(getProducts, { name: "$searchKey", filtersData: "$filtersData" })
+	wiredProducts;
+
+	@wire(MessageContext)
+	messageContext;
+
+	connectedCallback() {
+		this.subscribeMC();
+	}
+
+	disconnectedCallback() {
+		this.unsubscribeMC();
+	}
+
 	handleSearch(event) {
 		this.searchKey = event.detail.searchKey;
 	}
 
-	@wire(getProducts, { name: "$searchKey" })
-	wiredProducts;
+	subscribeMC() {
+		if (this.subscription) {
+			return;
+		}
+		this.subscription = subscribe(
+			this.messageContext,
+			FILTER_CHANNEL,
+			(message) => {
+				console.log("message " + JSON.stringify(message));
+				this.filtersData = message.filtersData;
+			}
+		);
+	}
+
+	unsubscribeMC() {
+		unsubscribe(this.subscription);
+		this.subscription = null;
+	}
 }
